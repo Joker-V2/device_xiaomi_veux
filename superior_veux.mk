75482a48a0cac331625dec390b8f922b8bb008e3
Joker-V2 <nipinna0@gmail.com>
2023-02-26 05:34:53 +0100

    veux: SuperiorOS bringup
    - Initial changes for superioros
    - Cleanup redundant PE specific flags

--- superior_veux.mk
+++ superior_veux.mk
@@ -8,19 +8,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-TARGET_USES_AOSP_RECOVERY := true
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_SUPPORTS_QUICK_TAP := true
 
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
 # Inherit some common PixelExperience
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common.mk)
 
-PRODUCT_NAME := aosp_veux
+PRODUCT_NAME := superior_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
